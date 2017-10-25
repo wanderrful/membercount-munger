a727d70816e16214dbc182bce792150d606bd18c
Wanderrful <galois@mentlegen.com>
2017-10-25 10:50:03 +0900

    further progress with refactoring as a class

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import ScrapeWorker from "./scrape_utils";
 
 
 
-let worker = new ScrapeWorker();
-worker.fn_login();
+let worker = new ScrapeWorker({
+    group_url: String(process.env.GROUP_URL),
+    check_interval: Number(process.env.CHECK_INTERVAL)
+});
+worker.fn_run();
--- src/scrape_utils.ts
+++ src/scrape_utils.ts
@@ -10,33 +10,37 @@ import * as pg from "pg";               // to record the data
 
 
 
-interface Config {
+interface IWorkerConfig {
+    group_url: string,
+    check_interval: number
+};
+interface IScrapeConfig {
     url: string,
-    model: any
+    model: IModelConfig
+};
+interface IModelConfig {
+    count: string,
+    ingame: string,
+    online: string
+};
+interface ITimeStamp {
+    date: string,
+    time: string
+};
+interface IRowData extends IModelConfig {
+    timestamp: ITimeStamp
 };
 
 
 
-////
-
-
-
 export default class ScrapeWorker extends pg.Client {
-    constructor() {
-        super({});
+    /// Class members
+    public group_url: string;
+    public check_interval: number;
 
-        this.on("error", (err) => {
-            console.error("*** DB ERROR: ", err.stack);
-        });
 
-        this.on('notice', (msg) => { 
-            console.warn('notice:', msg);
-        });
-    }
 
-    
-    
-    // PostgreSQL query strings
+    /// PostgreSQL query strings
     public db_initQuery: string = `
 CREATE TABLE IF NOT EXISTS db_membercounts(
     timestamp text not null primary key, count numeric not null, ingame numeric not null, online numeric not null
@@ -47,76 +51,128 @@ VALUES(
     $1
 )`;
 
+    
+    
+    /// Constructor
+    constructor(config: IWorkerConfig) {
+        super({}); //database login details are pulled automatically from environment variables!
+
+        this.on("error", (err) => {
+            this.fn_log("ERROR:", err.stack);
+        });
+
+        this.on('notice', (msg) => { 
+            this.fn_log('Notice:', msg);
+        });
+
+        this.group_url = config.group_url;
+        this.check_interval = config.check_interval;
+    }
 
 
-    // Class methods
-    fn_login(): void {
+
+    /// Class methods
+
+    // Utility function to make console.log have a consistent format
+    fn_log(text: string, args?: any): void {
+        console.log("*** DB ::  ", text, " :: ", args);
+    }
+
+    // Utility function that returns the current timestamp as a convenient JSON object
+    fn_getTimeStamp(): ITimeStamp {
+        let now: Date = new Date();
+        let date: Array<String> = [ String(now.getMonth() + 1), String(now.getDate()), String(now.getFullYear()) ];
+        let time: Array<String> = [ String(now.getHours()), String(now.getMinutes())];
+        for (let i of time) {  
+            if ( Number(i) < 10 ) {
+              i = "0" + i;
+            }
+        }
+        
+        return { 
+            date: date.join("/"),
+            time: time.join(":")
+        };
+    }
+
+
+
+    // Use this to access the database.  Called in the constructor.
+    fn_login(do_the_thing: () => void): void {
         // Attempt to connect to the PostgreSQL Database!
         this.connect((err) => {
             if (err) {
-                console.error("*** DB CONNECTION ERROR: ", err.stack);
+                this.fn_log("CONNECTION ERROR", err.stack);
             } else {
-                console.log("*** DB CONNECTION SUCCESSFUL!");
+                this.fn_log("CONNECTION SUCCESSFUL");
                 
-                // Initialize the Table we will use, if it does not exist
-                this.query({
-                    text: this.db_initQuery
-                }, (err)=>{
-                    console.error("*** QUERY ERROR: ", err.stack);
+                // Now that we are logged in, attempt to initialize the master table
+                do_the_thing();
+
+                this.end( (err) => {
+                    if (err) {
+                        this.fn_log("ERROR DURING DISCONNECTION", err.stack);
+                    }
+                    this.fn_log("DISCONNECTED");
                 });
             }
         });
     }
-}
 
+    // Initialize the master table, if it does not already exist.
+    fn_initMasterTable(): void {
+        this.query({
+            text: this.db_initQuery
+        }, (err, res) => {
+            if (err) {
+                this.fn_log("QUERY ERROR", err.stack);
+            }
+            this.end();
+        });
+    }
+
+    // Scrape the bot's assigned page to get and return the configured data.
+    fn_getGroupData(config: IScrapeConfig): void {
+        
+    }
+
+    // The main entry point for actually doing work
+    fn_run(): void {
+        // Configure the data object for scraping
+        let config: IScrapeConfig = {
+            url: this.group_url,
+            model: { 
+                count: ".content .membercounts .membercount.members .count",
+                ingame: ".content .membercounts .membercount.ingame .count",
+                online: ".content .membercounts .membercount.online .count"
+            }
+        };
+
+        scrapy.scrape(config.url, config.model, (err,data: IModelConfig) => {
+            if (err) return console.error(err);
+            
+            this.fn_login( () => {
+                this.fn_writeToDatabase({
+                    timestamp: this.fn_getTimeStamp(),
+                    ...data
+                })
+            });
+        });
+    }
+
+    fn_writeToDatabase(data: IRowData): void {
+        throw "Not yet implemented!";
+    }
+};
+//(Number(process.env.CHECK_INTERVAL) * 60 * 1000 ) )
 
 
 ////
 
 
 
-// Scrape function to get data.
-function fn_getGroupData(config: Config): void {
-    scrapy.scrape(config.url, config.model, (err,data) => {
-        if (err) return console.error(err);
-        
-        return {
-            timestamp: fn_getTimeStamp(),
-            ...data
-        };
-    });
-}
-
-// Returns a timestamp as an object.
-function fn_getTimeStamp(): Object {
-    let now: Date = new Date();
-    let date: Array<String> = [ String(now.getMonth() + 1), String(now.getDate()), String(now.getFullYear()) ];
-    let time: Array<String> = [ String(now.getHours()), String(now.getMinutes())];
-
-    for (let i of time) {  
-        if ( +(i) < 10 ) {
-          i = "0" + i;
-        }
-    }
-    // Return the formatted string
-    return { 
-        date: date.join("/"),
-        time: time.join(":")
-    };
-}
 
 
-
-
-
-// PostgreSQL functions
-function fn_connectToDB(): pg.Client {
-    const client = new pg.Client({});
-
-    
-
-    return client;
-}
 function fn_query(client: pg.Client, query: pg.QueryConfig|string, func: (args?: any) => any ): void {
     client.query(query, (err, res) => {
         if (err) {
@@ -129,27 +185,4 @@ function fn_query(client: pg.Client, query: pg.QueryConfig|string, func: (args?:
 }
 function fn_insert(client: pg.Client, query: pg.QueryConfig|string, func:(args?: any) => any): void {
     fn_query(client, query, func());
-}
-
-
-
-// Main entry point function
-export function fn_run(): void {
-    // Configure the data object for scraping
-    let config: Config = {
-        url: process.env.GROUP_URL,
-        model: { 
-            count: ".content .membercounts .membercount.members .count",
-            ingame: ".content .membercounts .membercount.ingame .count",
-            online: ".content .membercounts .membercount.online .count"
-        }
-    };
-
-    // Connect to the Database
-    const client = fn_connectToDB();
-
-    // Finally, begin the looping scrape for data
-    setInterval( () => {
-        let data = fn_getGroupData(config);
-    }, (+(process.env.CHECK_INTERVAL) * 60 * 1000 ) );
 }
