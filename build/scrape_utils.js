fe3e3eb1ccbd92d0cb9d985a7e0c8476fbb260bb
Wanderrful <galois@mentlegen.com>
2017-10-26 22:29:37 +0900

    Forgot to compile the previous commit!

--- build/scrape_utils.js
+++ build/scrape_utils.js
@@ -11,16 +11,14 @@ function fn_log(text, args = "--") {
 exports.fn_log = fn_log;
 function fn_getTimeStamp() {
     let now = new Date();
-    let date = [String(now.getMonth() + 1), String(now.getDate()), String(now.getFullYear())];
-    let time = [String((now.getHours() + 9) % 24)];
-    for (let i of time) {
-        if (Number(i) < 10) {
-            i = "0" + i;
-        }
+    let date = String(now.getDay());
+    let time = String((now.getHours() + 9) % 24);
+    if (Number(time) < 10) {
+        time = "0" + time;
     }
     return {
-        date: date.join("/"),
-        time: time.join(":")
+        date: date,
+        time: time
     };
 }
 exports.fn_getTimeStamp = fn_getTimeStamp;
